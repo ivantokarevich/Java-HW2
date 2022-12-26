dbe27d6533f798d51fe5675a3d33281e60d02925
Ivan <t.i.r.84@yandex.ru>
2022-12-26 14:58:37 +0300

    DZ2

---
+++ Task1.java
@@ -0,0 +1,26 @@
+public class Task1 {
+	public static void main(String[] args) {
+
+	}
+
+	public int maxAreaOfIsland(int[][] grid) {
+		int count = 0;
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if (grid[i][j] == 1) {
+					count = Math.max(count, dfs(i, j, grid));
+				}
+			}
+		}
+		return count;
+	}
+
+	private int dfs(int i, int j, int[][] grid) {
+		if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) {
+			return 0;
+		}
+
+		grid[i][j] = 0;
+		return 1 + dfs(i - 1, j, grid) + dfs(i + 1, j, grid) + dfs(i, j - 1, grid) + dfs(i, j + 1, grid);
+	}
+}
---
+++ Task3.java
@@ -0,0 +1,24 @@
+import javax.swing.tree.TreeNode;
+
+class Task3 {
+	public static void main(String[] args) {
+
+	}
+
+	public boolean isSymmetric(TreeNode root) {
+		if (root == null) {
+			return true;
+		}
+		return dfs(root.left, root.right);
+	}
+
+	public boolean dfs(TreeNode q, TreeNode p) {
+		if (q == null && p == null) {
+			return true;
+		}
+		if (q == null || p == null) {
+			return false;
+		}
+		return q.val == p.val && dfs(q.left, p.right) && dfs(q.right, p.left);
+	}
+}
